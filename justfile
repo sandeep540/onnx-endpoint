db09e3be95ae612d3fb3b5b567ebc71372d7423a
sandeep540 <kn.sandeep@gmail.com>
2024-07-10 16:09:54 +0530

    Initial Commit

---
+++ justfile
@@ -0,0 +1,40 @@
+# Justfile for FastAPI Project
+
+# Install project dependencies using Poetry
+style:
+    black .
+    isort .
+    flake8
+
+install:
+	pip install -U pip
+	pip install -r requirements.txt
+	pip install -e .
+	pre-commit install
+
+# Run the FastAPI development server
+run:
+    uvicorn main:app --reload
+
+# Run tests using Poetry and pytest
+test:
+    pytest
+
+# Clean up generated files
+clean:
+	rm -rf `find . -name __pycache__`
+	rm -f `find . -type f -name '*.py[co]' `
+	rm -f `find . -type f -name '*~' `
+	rm -f `find . -type f -name '.*~' `
+	rm -rf .cache
+	rm -rf .pytest_cache
+	rm -rf htmlcov
+	rm -rf *.egg-info
+	rm -f .coverage
+	rm -f .coverage.*
+	rm -rf build
+
+# Run the development environment (install, run, and watch for changes)
+dev:
+    just install
+    just run
---
+++ setup.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from setuptools import find_namespace_packages, setup
+
+# load libraries from requirements.txt
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt")) as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+docs_packages = [
+    "mypy==1.5.1",
+    "mkdocs-material==9.3.1",
+    "mkdocstrings-python==1.7.0"
+]
+
+style_packages = [
+    "black==23.12.1",
+    "flake8==7.0.0",
+    "isort==5.13.2"
+]
+
+# define the package
+setup(
+    name="onnx-model-serving",
+    version="0.1",
+    description="Serving simple ONNX Model as REST Endpoint",
+    author="Sandeep Kongathi",
+    author_email="kn.sandeep@gmail.com",
+    url="https://dwiuzila.github.io/tagolym-ml/",
+    python_requires=">=3.9",
+    packages=find_namespace_packages(),
+    install_requires=[required_packages],
+    extras_require={
+        "dev": docs_packages + style_packages,
+        "docs": docs_packages,
+    },
+)
